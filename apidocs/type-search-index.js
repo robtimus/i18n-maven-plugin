9b0f9f174f5f575ea2c7b636507ab1e7f2c761b5
robtimus <robtimus@users.noreply.github.com>
2026-09-25 15:24:34 +0000

    Updated for version 4.0

---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.github.robtimus.maven.plugins.i18n","l":"GenerateMojo"},{"p":"com.github.robtimus.maven.plugins.i18n","l":"Scope"}]
